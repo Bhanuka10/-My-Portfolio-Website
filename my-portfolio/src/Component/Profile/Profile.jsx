f55a41fee88601857ccf93af562ee6b41c2a51b8
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-09-21 01:34:10 +0530

    edit profile

--- my-portfolio/src/Component/Profile/Profile.jsx
+++ my-portfolio/src/Component/Profile/Profile.jsx
@@ -8,15 +8,15 @@ const Profile = () => {
             
                 
                 <div className="profile-pic-container">
-                    <img className="profile-pic" src="src/assets/istockphoto-625389694-612x612-removebg-preview.png" alt="" />
+                    <img className="profile-pic" src="src/assets/WhatsApp_Image_2025-04-04_at_09.30.12_89ba444b-removebg-preview.png" alt="" />
                 </div>
             
         </div>
         <div className="profile-content">
             <h1>Hi There</h1>
-            <h2>I am <span>John Doe</span></h2>
-            <h3>Frontend Developer</h3>
-            <p>Creating amazing digital experiences with modern technology and creative design solutions.</p>
+            <h2>I am <span>Bhanuka Bandaranayake</span></h2>
+            <h3>Fullstack Developer</h3>
+            <p>Passionate Front-End Developer and back-End Developer| Aspiring ML & AI Enthusiast | Diploma in English |Undergraduate in Computing and Information Systems</p>
             <button className='btn'>Download CV</button>
         </div>
     </div>
