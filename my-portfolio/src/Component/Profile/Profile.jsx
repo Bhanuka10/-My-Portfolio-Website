115236f794ea2ed1c0af9c98e656c2ef105f069e
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-09-21 13:54:55 +0530

    change the profile part

--- my-portfolio/src/Component/Profile/Profile.jsx
+++ my-portfolio/src/Component/Profile/Profile.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import './Profile.css'
+import TypedText from '../TypedText/TypedText'
 
 const Profile = () => {
+  const roles = [
+    "Fullstack Developer",
+    "Aspiring ML & AI Enthusiast",
+    "Lifelong Learner"
+  ];
+
   return (
     <div className='profile'>
         <div className="profile-stage">
-            
-                
-                <div className="profile-pic-container">
-                    <img className="profile-pic" src="src/assets/WhatsApp_Image_2025-04-04_at_09.30.12_89ba444b-removebg-preview.png" alt="" />
-                </div>
-            
+            <div className="profile-pic-container">
+                <img className="profile-pic" src="src/assets/WhatsApp_Image_2025-04-04_at_09.30.12_89ba444b-removebg-preview.png" alt="" />
+            </div>
         </div>
         <div className="profile-content">
             <h1>Hi There</h1>
-            <h2>I am <span>Bhanuka Bandaranayake</span></h2>
-            <h3>Fullstack Developer</h3>
-            <p>Passionate Front-End Developer and back-End Developer| Aspiring ML & AI Enthusiast | Diploma in English |Undergraduate in Computing and Information Systems</p>
+            <h2>It's Me <span>Bhanuka Bandaranayake</span></h2>
+            <h3>I'm a <span><TypedText textArray={roles} typingSpeed={80} deletingSpeed={40} delayBetweenTexts={1500} /></span></h3>
+            <p>"I'm an undergraduate computing student passionate about building scalable web apps and AI-powered solutions."</p>
             <button className='btn'>Download CV</button>
         </div>
     </div>
