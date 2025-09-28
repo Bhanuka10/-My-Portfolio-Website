cb44da967ac9dad13432f281739c68f4255dcc6c
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-09-28 19:49:59 +0530

    add Latest Movies Library

--- my-portfolio/src/Data/projectsData.js
+++ my-portfolio/src/Data/projectsData.js
@@ -15,6 +15,15 @@ const projectsData = [
     techStack: ["Laravel", "PHP", "MySQL", "Blade", "HTML", "CSS", "JavaScript","git"],
     link: "https://github.com/Bhanuka10/Hostel-Management-System-Laravel-"
   },
+  {
+  id: 3,
+  title: "Latest Movies Library",
+  description: "A React-based web application that fetches and displays popular movies using a movie API. The app presents movie posters, titles, and details in a responsive layout with smooth navigation. It helps users explore trending films in an interactive and visually engaging way.",
+  image: "/images/moviesapp.png",
+  techStack: ["React", "JavaScript", "HTML", "CSS", "API Integration", "Git"],
+  link: "https://github.com/Bhanuka10/Movies-collection"
+}
+
 ];
 
 export default projectsData;
