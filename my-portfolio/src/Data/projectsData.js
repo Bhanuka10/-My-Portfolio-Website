c42bcb546a549b1591dba54efe51d4946061846f
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-09-28 22:54:56 +0530

    add capston project

--- my-portfolio/src/Data/projectsData.js
+++ my-portfolio/src/Data/projectsData.js
@@ -3,7 +3,7 @@ const projectsData = [
     id: 1,
     title: "Personalized Learning Web Application",
     description: "Personalized Learning Web Application designed to help learners become experts in their chosen field. The system analyzes user profiles—such as educational background, skills, and learning goals—and recommends the most relevant online courses and resources.",
-    image: "/images/agriwave.png",
+    image: "/src/assets/Cpstone/Screenshot (161).png",
     techStack: ["React", "Firebase", "JavaScript","HTML","CSS","Gemini API","git","figma"],
     link: "https://github.com/Bhanuka10/Capstone"
   },
@@ -11,7 +11,7 @@ const projectsData = [
     id: 2,
     title: "Hostel Management System",
     description: "A Laravel-based Hostel Management System that streamlines operations by managing student registration, room allocation, fee tracking, and staff records. It provides administrators with reporting and payment tools, while students can easily view their details, reducing manual work and improving efficiency.",
-    image: "/images/traveltailor.png",
+    image: "/src/assets/hostel_management_System/first one(1).png",
     techStack: ["Laravel", "PHP", "MySQL", "Blade", "HTML", "CSS", "JavaScript","git"],
     link: "https://github.com/Bhanuka10/Hostel-Management-System-Laravel-"
   },
