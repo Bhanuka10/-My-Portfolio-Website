eb8e819843d7771d6ab6f2e357179719457a90e8
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-09-29 18:32:07 +0530

    project jsx file

--- my-portfolio/src/Component/Projects/Projects.jsx
+++ my-portfolio/src/Component/Projects/Projects.jsx
@@ -4,10 +4,12 @@ import projectsData from "../../Data/projectsData";
 
 const Projects = () => {
   return (
-    <div className="portfolio">
-      {projectsData.map((project) => (
-        <div className="project-card" key={project.id}>
-          <img src={project.image} alt={project.title} className="project-image" />
+<div className="portfolio">
+  {projectsData.map((project) => (
+    <div className="project-card" key={project.id}>
+      <div className="image-container">
+        <img src={project.image} alt={project.title} className="project-image" />
+        <div className="overlay">
           <h3>{project.title}</h3>
           <p>{project.description}</p>
           <div className="tech-stack">
@@ -15,12 +17,15 @@ const Projects = () => {
               <span key={index} className="tech">{tech}</span>
             ))}
           </div>
-          <a href={project.link} target="_blank" rel="noopener noreferrer">
+          <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
             View Project
           </a>
         </div>
-      ))}
+      </div>
     </div>
+  ))}
+</div>
+
   );
 };
 
