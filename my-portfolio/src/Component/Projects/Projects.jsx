b463af64bce69c96725578bf38bc04060b4a9e80
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-09-29 22:31:32 +0530

    new way to project

--- my-portfolio/src/Component/Projects/Projects.jsx
+++ my-portfolio/src/Component/Projects/Projects.jsx
@@ -7,20 +7,25 @@ const Projects = () => {
 <div className="portfolio">
   {projectsData.map((project) => (
     <div className="project-card" key={project.id}>
-      <div className="image-container">
-        <img src={project.image} alt={project.title} className="project-image" />
-        <div className="overlay">
-          <h3>{project.title}</h3>
-          <p>{project.description}</p>
-          <div className="tech-stack">
-            {project.techStack.map((tech, index) => (
-              <span key={index} className="tech">{tech}</span>
-            ))}
-          </div>
-          <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
-            View Project
-          </a>
+      
+        <div className="project-image-container">
+          <img src={project.image} alt={project.title} className="project-image" />
         </div>
+
+
+      <div className="project-details">
+        <div className="project-header">
+          <h3 className="project-title">{project.title}</h3>
+        </div>
+        <p className="project-description">{project.description}</p>
+        <div className="project-links">
+          {project.github && (
+            <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
+              GitHub
+            </a>
+          )}
+        </div>
+
       </div>
     </div>
   ))}
