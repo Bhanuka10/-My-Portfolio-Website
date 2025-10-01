e8051582dc4788f66c28e2dd48eec0d1618ebb75
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-10-01 22:38:21 +0530

    add details to the Ui/Ux part

--- my-portfolio/src/Data/projectsData.js
+++ my-portfolio/src/Data/projectsData.js
@@ -25,5 +25,40 @@ const projectsData = [
 }
 
 ];
+const UisData=[
+  {
+    id: 1,
+    title: "Capstone Interface Design",
+    description: "The UI/UX design of Skill Forge was created using Figma, focusing on delivering a clean, modern, and user-friendly experience for learners. The design process followed a user-centered approach, ensuring intuitive navigation and accessibility across devices",
+
+    indetails:"The UI/UX design of Skill Forge was created using Figma, focusing on delivering a clean, modern, and user-friendly experience for learners. The design process followed a user-centered approach, ensuring intuitive navigation and accessibility across devices. Key features include a personalized dashboard, course catalog with filtering options, progress tracking, and interactive elements to enhance engagement. The color scheme and typography were chosen to create a visually appealing interface that aligns with the brand identity. Overall, the design aims to provide an enjoyable learning journey for users.",
+
+    image: "src/assets/UI/Capstone/Desktop - 1.png",
+    techStack: ["Figma"],
+    explore_link: "https://github.com/Bhanuka10/UI-Designs"
+  },
+
+  {
+    id: 2,
+    title: "Wet",
+    description: "I created the UI/UX design for the WET Hospital system in Figma, ensuring a clear and professional interface for both patients and administrators. The design includes pages such as the home page, apply for medicine, doctor dashboard all connected with interactive prototypes to reflect a smooth hospital workflow.",
+    indetails:"The UI/UX design for the WET Hospital system was meticulously crafted using Figma to ensure a clear and professional interface for both patients and administrators. The design encompasses key pages such as the home page, application for medicine, and doctor dashboard, all interconnected with interactive prototypes to facilitate a seamless hospital workflow. Emphasizing usability and accessibility, the design incorporates intuitive navigation, consistent color schemes, and clear typography to enhance user experience. The goal was to create an efficient and user-friendly platform that meets the needs of its diverse users while maintaining a cohesive visual identity.",
+    image: "src/assets/UI/wet/Home page.png",
+    techStack: ["Figma"],
+    explore_link: "https://github.com/Bhanuka10/Mobile-App-UI-Kit"
+  }
+
+  {
+    id: 3,
+    title: "Learning web",
+    description: "A modern and user-friendly UI/UX design for a learning web application, created using Figma. The design focuses on intuitive navigation, clean layouts, and engaging visuals to enhance the user experience for learners.",
+    indetails:"A modern and user-friendly UI/UX design for a learning web application, created using Figma. The design focuses on intuitive navigation, clean layouts, and engaging visuals to enhance the user experience for learners. Key features include a personalized dashboard, course catalog with filtering options, progress tracking, and interactive elements to boost engagement. The color scheme and typography were carefully selected to create a visually appealing interface that aligns with the brand identity. Overall, the design aims to provide an enjoyable and effective learning journey for users.",
+    image: "src/assets/UI/class/Desktop - 2.png",
+    techStack: ["Figma"],
+    explore_link: "https://github.com/Bhanuka10/Mobile-App-UI-Kit"
+
+    
+  }
+]
 
 export default projectsData;
